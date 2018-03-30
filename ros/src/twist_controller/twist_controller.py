dfb315c5629b810dc3d12fc231556728acc88624
Pepar Hugo <peparhugo@gmail.com>
2018-03-30 09:20:02 -0700

    changed max throttle and brake percentage to 0.4

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -10,8 +10,8 @@ class Controller(object):
         # TODO: Implement
         self.sample_time = 1. / freq
         self.yaw_controller = yaw_controller
-        self.throttle_pid = PID(self.sample_time * 1.5, 0.002, 0.0001, 0., 1.)
-        self.brake_pid = PID(self.sample_time, 0.002, 0.0001, 0., 1.)
+        self.throttle_pid = PID(self.sample_time * 1.5, 0.002, 0.0001, 0., 0.4)
+        self.brake_pid = PID(self.sample_time, 0.002, 0.0001, 0., 0.4)
 
     def control(self, linear_velocity, angular_velocity, current_velocity):
         # TODO: Change the arg, kwarg list to suit your needs
